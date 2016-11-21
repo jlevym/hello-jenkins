82ba6f4642ca0e6e26f2bdb73d965f816cfa960e
jlevym <jlevym@gmail.com>
2016-11-21 21:23:50 +0200

    gistill not working

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-	res.send('hello wro jenkins');
+	res.send('hello wr jenkins');
 });
 
 /*app.listen(process.env.PORT || 5000);*/
